bc918ab8edc3d5735802bd6de9141fa461580266
Takeru Ohta <phjgt308@gmail.com>
2018-01-07 14:16:07 +0900

    Add basic test

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,26 @@ pub use defaults::{DefaultCpuTaskQueue, DefaultIoTaskQueue};
 
 mod async;
 mod defaults;
+
+#[cfg(test)]
+mod test {
+    use futures::{Async, Future};
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let future = DefaultCpuTaskQueue.async_call(|| 1 + 1);
+        assert_eq!(wait(future), Ok(2));
+
+        let future = DefaultIoTaskQueue.async_call(|| -> () { panic!() });
+        assert_eq!(wait(future), Err(AsyncCallError));
+    }
+
+    fn wait<F: Future>(mut f: F) -> Result<F::Item, F::Error> {
+        loop {
+            if let Async::Ready(v) = f.poll()? {
+                return Ok(v);
+            }
+        }
+    }
+}
